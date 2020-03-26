45a6a9ecee8046d6dce9fa0ab1c438069c01e08a
Baburin Vladislav <vlad.babur.vb@gmail.com>
2020-03-26 05:07:38 +0300

    lab2 changed

--- Parking/src/dao/CarDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import domain.Car;
-import domain.CarOwner;
-
-public interface CarDAO extends GenericDAO<Car> {
-
-    Car findByPlate(String plate);
-}
--- Parking/src/dao/ParkingDAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import domain.Parking;
-
-public interface ParkingDAO extends GenericDAO<Parking> {
-
-    Parking findByParkingName(String parkingName);
-}
